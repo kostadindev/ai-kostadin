007bb43a450730d560e5620159accc8e72b6a6f7
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2025-04-06 15:47:21 -0400

    add ngrok url for app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      '5ed1-2600-4808-5931-a900-a48a-5409-62bc-5c0b.ngrok-free.app'
+    ]
+  }
 })
