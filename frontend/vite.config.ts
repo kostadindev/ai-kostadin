0a2c35ebc1c84e6dc68cac6f147885c7f1598698
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2025-04-09 22:27:51 -0400

    add dockerfile dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: true, // crucial for Docker: allows binding to 0.0.0.0
+    port: 5173, // explicit port helps with Docker/Compose
+    strictPort: true, // avoid port fallback (important if exposing specific port)
     allowedHosts: [
       'localhost',
       '5ed1-2600-4808-5931-a900-a48a-5409-62bc-5c0b.ngrok-free.app'
@@ -14,4 +16,4 @@ export default defineConfig({
       'ngrok-skip-browser-warning': 'true'
     }
   }
-})
+});
