414ccd9de2c641f7a367dab9f50a2ae929bf460f
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2025-04-09 21:41:04 -0400

    add docker for client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Chat from "./components/chat";
 import { ConfigProvider, theme } from "antd";
 
